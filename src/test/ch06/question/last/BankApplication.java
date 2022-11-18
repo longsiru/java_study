7cceb7a58b370d9c26cb632300b0d54c2e31fa0e
longsiru <sirulong89@gmail.com>
2022-11-18 18:25:18 +0900

    up

--- src/test/ch06/question/last/BankApplication.java
+++ src/test/ch06/question/last/BankApplication.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class BankApplication {
 	private static Scanner scanner = new Scanner(System.in); //스캐너 객체 생성
 	
-	//게좌갯체를 담을 배열
+	//게좌객체를 담을 배열
 	private static Account[] accountArray = new Account[100]; 
 
 	
@@ -70,13 +70,36 @@ public class BankApplication {
 	}
 	
 	//3. 예금
-	public static void despoit() {}
+	public static void despoit() {
+		//계좌번호, 예금액 입력받아(scanner)
+		//계좌번호로 계좌를 찾아야 한다.
+		//Account account = findAccount(ano) //호출
+		//찾는 계좌에 예금을 해준다.
+	    //account.setBalance(account.getBalance() + 예금예);
+	}
 	
 	//4. 출금
-	public static void withdraw() {}
+	public static void withdraw() {
+		//계좌번호, 예금액 입력받아(scanner)
+		//계좌번호로 계좌를 찻자야 한다.
+		//찾는 계좌에 출금을 해준다.
+	}
 	
 	//5. accountArray 배열에서 ano와 동일한 Account객체 찾는 역할을 한다.
 	private static Account findAccount(String ano) {
+		Account account = null;
 		
+		for(int i = 0; i < accountArray.length; i++) {
+			if (accountArray[i]!= null) {  //null이니면 계좌가 있다느 말
+				//불러온 계좌안에 있는 계좌 번호와 매개변수로 받아온 계좌번호 무ㅐ가 같은지 확인
+				String dbAno = accountArray[i].getAno();
+				
+				if(dbAno.equals(ano)) {
+					account = accountArray[i];
+					break;
+				}
+			}
+		}
+		return account;  //在for文的括号后return。
 	}
 }
