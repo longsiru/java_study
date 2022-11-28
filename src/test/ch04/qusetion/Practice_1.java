e3264ffe145de4904a453227d06bf861664fd80e
longsiru <sirulong89@gmail.com>
2022-11-28 10:44:42 +0900

    up

--- src/test/ch04/qusetion/Practice_1.java
+++ src/test/ch04/qusetion/Practice_1.java
@@ -1,5 +1,7 @@
 package test.ch04.qusetion;
 
+import java.util.Scanner;
+
 public class Practice_1 {
 
 	public static void main(String[] args) {
@@ -15,10 +17,8 @@ public class Practice_1 {
 //        7. boolean형 변수 powerOn가 false일 때 true인 조건식:  !powerOn  
 //        8. 문자열 참조변수 str이 “yes”일 때 true인 조건식: str.equals("yes")
 
-		
-		
 //		2. 2.	for 문을 이용하여 1~100까지의 정수 중 3의 배수 총합을 구하는 코드를 작성하시오.
-		
+
 //		int sum = 0;
 //		for(int i=1; i<=100; i++) {
 //			if(i % 3 == 0) {
@@ -27,15 +27,9 @@ public class Practice_1 {
 //			}
 //		}
 //		System.out.println("sum=" + sum);  //sum=1683
-		
-		
-		
-		
-		
-		
-		
+
 //		3.	1부터 20까지의 정수 중에서 2 또는 3의 배수가 아닌 수의 총합을 구하는 코드를 작성하시오
-		
+
 //		int sum = 0;
 //		for(int i=1; i<=20; i++) {
 //			if((i %2 != 0) && (i %3 != 0)) {
@@ -44,15 +38,11 @@ public class Practice_1 {
 //			}
 //		}
 //		System.out.println("sum=" + sum);
-		
-		
-		
+
 //		4.	while 문과 Math.random( ) 메소드를 이용해서 두 개의 주사위를 던졌을 때 나오는 눈을 (눈1,
 //				눈2) 형태로 출력하고, 눈의 합이 5가 아니면 계속 주사위를 던지고, 눈의 합이 5이면 실행을 멈추
 //				는 코드를 작성해보세요. 눈의 합이 5가 되는 경우는 (1, 4), (4, 1), (2, 3), (3, 2)입니다.
 
-		
-		
 //		while (true) {
 //			int num1 = (int) (Math.random() * 6) + 1;
 //			int num2 = (int) (Math.random() * 6) + 1;
@@ -62,7 +52,7 @@ public class Practice_1 {
 //			}   
 //		}
 //		
-		
+
 //		5.	중첩 for 문을 이용하여 방정식 4x + 5y = 60의 모든 해를 구해서 (x, y) 형태로 출력하는 코드
 //				를 작성해보세요. 단, x와 y는 10 이하의 자연수입니다
 
@@ -76,10 +66,9 @@ public class Practice_1 {
 //			}
 //			
 //		}
-		
-		
-		//6.	1+(-2)+3+(-4)+... 과 같은 식으로 계속 더해나갔을 때, 몇까지 더해야 
-		//총합이 100이상이 되는지 구하는 프로그램을 작성하시오.
+
+		// 6. 1+(-2)+3+(-4)+... 과 같은 식으로 계속 더해나갔을 때, 몇까지 더해야
+		// 총합이 100이상이 되는지 구하는 프로그램을 작성하시오.
 //		int sum = 0;
 //		int s =1;
 //		int num = 0;
@@ -91,23 +80,20 @@ public class Practice_1 {
 //		}
 //		System.out.println("num= " + num);
 //		System.out.println("sum= " + sum);
-		
-		
-		
-		
-		//7.	두 개의 주사위를 던졌을 때, 눈의 합이 6이 되는 모든 경우의 수를 출력하는 
-		//프로그램을 작성하시오.(이중 for문 이용)
-		
+
+		// 7. 두 개의 주사위를 던졌을 때, 눈의 합이 6이 되는 모든 경우의 수를 출력하는
+		// 프로그램을 작성하시오.(이중 for문 이용)
+
 //		for (int i = 1; i <=6; i++) {
 //			for (int j = 1; j <= 6; j++)
 //				if (i + j == 6)
 //					System.out.println(i + "+" + j + "=" + (i + j));
 //		}
-		
-		
-		
-		/* 8.	int타입의 변수 num 이 있을 때, 각 자리의 합을 더한 결과를 출력하는 코드를 완성하라. 
-		 * 만일 변수 int num의 값이 12345라면, ‘1+2+3+4+5’의 결과인 15를 출력하라. (문자 변환 금지) */
+
+		/*
+		 * 8. int타입의 변수 num 이 있을 때, 각 자리의 합을 더한 결과를 출력하는 코드를 완성하라. 만일 변수 int num의 값이
+		 * 12345라면, ‘1+2+3+4+5’의 결과인 15를 출력하라. (문자 변환 금지)
+		 */
 //		int num = 12345;
 //		int sum = 0;
 //		while (num > 0) {
@@ -115,12 +101,12 @@ public class Practice_1 {
 //			num /= 10;
 //		}
 //		System.out.println("sum= " + sum);
-		
-		
-		/*9.	피보나치(Fibonnaci) 수열(數列)은 앞을 두 수를 더해서 다음 수를 만들어 나가는 수열이다. 
-		 * 예를 들어 앞의 두 수가 1과 1이라면 그 다음 수는 2가 되고 그 다음 수는 1과 2를 더해서 3이 
-		 * 되어서 1,1,2,3,5,8,13,21,... 과 같은 식으로 진행된다. 1과 1부터시작하는 피보나치수열의 10번째 
-		 * 수는 무엇인지 계산하는 프로그램을 완성하시오. */
+
+		/*
+		 * 9. 피보나치(Fibonnaci) 수열(數列)은 앞을 두 수를 더해서 다음 수를 만들어 나가는 수열이다. 예를 들어 앞의 두 수가 1과
+		 * 1이라면 그 다음 수는 2가 되고 그 다음 수는 1과 2를 더해서 3이 되어서 1,1,2,3,5,8,13,21,... 과 같은 식으로
+		 * 진행된다. 1과 1부터시작하는 피보나치수열의 10번째 수는 무엇인지 계산하는 프로그램을 완성하시오.
+		 */
 //		int num1 = 1;
 //		int num2 = 1;
 //		int num3 = 0;
@@ -132,41 +118,83 @@ public class Practice_1 {
 //			num2 = num3;
 //		}
 //		
-		
-		
-		
-		/*10.	for 문을 이용해서 다음과 같은 실행 결과가 나오는 코드를 작성해보세요.(이중 for문 이용) */
-		 
-//		  for (int i=1; i<=5; i++) {
-//			  for(int k=1; k<=i; k++) {
-//				System.out.println("*");
-//				if( k == i ) {
-//				  System.out.println(); 
-//			      }
-//			   }
-//		  }
+
+		/* 10. for 문을 이용해서 다음과 같은 실행 결과가 나오는 코드를 작성해보세요.(이중 for문 이용) */
+
+//		for (int i = 1; i <= 5; i++) {
+//			for (int k = 1; k <= i; k++) {
+//				System.out.print("*");
+//				if (k == i) {
+//					System.out.println();
+//				}
+//			}
+//		}
+
+		/*
+		 * 11. 다음은 숫자맞추기 게임을 작성한 것이다. 1과 100사이의 값을 반복적으로 입력해서 컴퓨터가 생각한 값을 맞추면 게임이 끝난다.
+		 * 사용자가 값을 입력하면, 컴퓨터는 자신이 생각한 값과 비교해서 결과를 알려준다. 사용자가 컴퓨터가 생각한 숫자를 맞추면 게임이 끝나고 몇
+		 * 번 만에 숫자를 맞췄는지 알려준다. 네모에 알맞은 코드를 넣어 프로그램을 완성하시오.
+		 */
+
+//		int answer = (int)(Math.random()*100)+1;
+//		int input = 0;
+//		int count = 0;
 //		
-		
-		/* 11.	다음은 숫자맞추기 게임을 작성한 것이다. 1과 100사이의 값을 반복적으로 입력해서 컴퓨터가 생각한 
-		 * 값을 맞추면 게임이 끝난다. 사용자가 값을 입력하면, 컴퓨터는 자신이 생각한 값과 비교해서 결과를 알려준다. 
-		 * 사용자가 컴퓨터가 생각한 숫자를 맞추면 게임이 끝나고 몇 번 만에 숫자를 맞췄는지 알려준다. 
-		 * 네모에 알맞은 코드를 넣어 프로그램을 완성하시오.*/
-		
-		
-		
-		/* 12.	while 문과 Scanner의 nextLine ( ) 메소드를 이용해서 다음 실행 결과와 같이 키보드로부터
-		 * 입력된 데이터로 예금, 출금, 조회, 종료 기능을 제공하는 코드를 작성해보세요*/
-		
-		
-		
-		
-		
-		
-		
+//		Scanner scan = new Scanner(System.in);
+//		do {
+//			count++;
+//			System.out.println("1과 100사이의 값을 입력하세요");
+//			input = scan.nextInt();
+//			if(answer > input) {
+//				System.out.println("더 큰 숫자 입력하세요");
+//			}else if(answer < input) {
+//				System.out.println("작은 숫자 입력하세요");
+//			}else {
+//				System.out.println("맞췄습니다");
+//				System.out.println("시도횟수: " + count);
+//				break;
+//			}
+//		} while(true);
 		
 		
+		/*
+		 * 12. while 문과 Scanner의 nextLine ( ) 메소드를 이용해서 다음 실행 결과와 같이 키보드로부터 입력된 데이터로 예금,
+		 * 출금, 조회, 종료 기능을 제공하는 코드를 작성해보세요
+		 */
+		boolean run = true;
+		int balance = 0;
+		Scanner scan1 = new Scanner(System.in);
+		while(run) {
+			System.out.println("-------------------------------------");
+			System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
+			System.out.println("-------------------------------------");
+			System.out.println("선택> ");
+			int meunNum = scan1.nextInt();
+			switch(meunNum) {
+			case 1:
+				System.out.println("예금액> ");
+				balance += scan1.nextInt();
+				break;
+			case 2:
+				System.out.println("출금액> ");
+				balance -= scan1.nextInt();
+				break;
+			case 3:
+				System.out.println("잔고> ");
+				System.out.println(balance);
+				break;
+			case 4:
+				run = false;
+				break;
+			}
+			System.out.println();
+			
+		}
+		System.out.println("프로그램 종료");
 		
 		
+		
+
 	}
 
 }
